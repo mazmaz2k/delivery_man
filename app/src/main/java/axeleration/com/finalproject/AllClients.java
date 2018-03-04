d8b151992c7eaef806f3a7cc3054f5ab7d9571f7
Zvukamar <fre3run@gmail.com>
2018-03-04 14:52:42 +0200

    Added some comments

--- app/src/main/java/axeleration/com/finalproject/AllClients.java
+++ app/src/main/java/axeleration/com/finalproject/AllClients.java
@@ -3,17 +3,13 @@ package axeleration.com.finalproject;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+/* This class for showing all clients activity. */
 public class AllClients extends AppCompatActivity {
-
 
     private ListView list;
     private SQLiteDatabase db;
@@ -25,31 +21,40 @@ public class AllClients extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_clients);
 
-        db = DBHelperSingleton.getInstanceDBHelper(this).getReadableDatabase();
+        db = DBHelperSingleton.getInstanceDBHelper(this).getReadableDatabase(); // open db from singleton object.
 
-        list = findViewById(R.id.listView);
-        cursor = db.query(Constants.CLIENTS.TABLE_NAME, null, null, null, null, null, null);
-        adapter = new ClientCursorAdapter(this, cursor);
-        list.setAdapter(adapter);
+        list = findViewById(R.id.listView); // get a list view.
+        cursor = db.query(Constants.CLIENTS.TABLE_NAME, null, null, null, null, null, null);    // show all client from db.
+        adapter = new ClientCursorAdapter(this, cursor);    // initial the custom cursor adapter.
+        list.setAdapter(adapter);   // set adapter to the list view.
     }
 
+    /* Menu options initialize */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.client_navigation, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    /* Option choose for a menu  */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.sort_by_name:
+            case R.id.sort_by_name: // choose the sort by name option.
                 cursor = db.query(Constants.CLIENTS.TABLE_NAME,null,null,null,null,null,Constants.TASKS.FULL_NAME + " ASC");
                 break;
-            default:
-                onBackPressed();
+            default:    // choose the back button option.
+                onBackPressed();    // go to the previous screen.
         }
+        /* real time update the list view */
         adapter = new ClientCursorAdapter(this,cursor);
         list.setAdapter(adapter);
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
     }
 }
--- app/src/main/java/axeleration/com/finalproject/TaskCursorAdapter.java
+++ app/src/main/java/axeleration/com/finalproject/TaskCursorAdapter.java
@@ -8,7 +8,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,62 +16,61 @@ import android.widget.CursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/* Custom cursor adapter class */
 public class TaskCursorAdapter extends CursorAdapter {
 
     private LayoutInflater inflater;
 
+    /* Constructor */
     TaskCursorAdapter(Context context, Cursor c) {
-
         super(context, c, false);
         inflater = LayoutInflater.from(context);
     }
 
+    /* New view of the row with the data of list view */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return inflater.inflate(R.layout.task_item, parent, false);
     }
 
+    /* Manipulation for each row in the list view */
     @Override
-    public void bindView(View view, final Context context, final Cursor cursor) {// TODO: Add name of the client to the sms send.
-        TextView text = view.findViewById(R.id.name);
-        TextView phone = view.findViewById(R.id.phone);
-//        TextView date =view.findViewById(R.id.date);
-        TextView time =view.findViewById(R.id.time);
-        final String receiverDate = cursor.getString(cursor.getColumnIndex(Constants.TASKS.DATE));
-        final String reciverTime= cursor.getString(cursor.getColumnIndex(Constants.TASKS.DATETIME));
-        final int receiver_id = cursor.getInt(cursor.getColumnIndex(Constants.TASKS._ID));
-        final String receiver_phone_number = cursor.getString(cursor.getColumnIndex(Constants.TASKS.PHONE_NUMBER));
-        final String receiver_name = cursor.getString(cursor.getColumnIndex(Constants.TASKS.FULL_NAME));
-        final int id = cursor.getInt(cursor.getColumnIndex(Constants.TASKS.CLIENT_ID));
-        final String receiverPhoneNumber = cursor.getString(cursor.getColumnIndex(Constants.TASKS.PHONE_NUMBER));
-        final String receiverName = cursor.getString(cursor.getColumnIndex(Constants.TASKS.FULL_NAME));
-        Button navigate=view.findViewById(R.id.nevigateToLoacationBtn);
-        Button callBtm = view.findViewById(R.id.call);
-        final String address = cursor.getString(cursor.getColumnIndex(Constants.TASKS.ADDRESS));
-        Button doneTaskBtn= view.findViewById(R.id.finishedThisTaskBtn);
+    public void bindView(View view, final Context context, Cursor cursor) {
+        final TextView text = view.findViewById(R.id.name); // Text view of the name.
+        final TextView phone = view.findViewById(R.id.phone); // Text view of the phone number.
+        final TextView time = view.findViewById(R.id.time); // Text view of the date and time.
+        final Button navigate = view.findViewById(R.id.nevigateToLoacationBtn); // Button to make a navigation.
+        final Button callBtm = view.findViewById(R.id.call);    // Button to make a call.
+        final Button doneTaskBtn= view.findViewById(R.id.finishedThisTaskBtn); // Button to finish a task.
+
+        final String receiverTime = cursor.getString(cursor.getColumnIndex(Constants.TASKS.DATETIME));  // String for date and time.
+        final int receiver_id = cursor.getInt(cursor.getColumnIndex(Constants.TASKS._ID));  //  Int for receiver id number.
+        final int id = cursor.getInt(cursor.getColumnIndex(Constants.TASKS.CLIENT_ID)); // Client ID number.
+        final String receiverPhoneNumber = cursor.getString(cursor.getColumnIndex(Constants.TASKS.PHONE_NUMBER));   // Receiver phone number string.
+        final String receiverName = cursor.getString(cursor.getColumnIndex(Constants.TASKS.FULL_NAME)); // Receiver name string.
+        final String receiverAddress = cursor.getString(cursor.getColumnIndex(Constants.TASKS.ADDRESS));    // Receiver address string.
         doneTaskBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {  // On click listener to finish the task and goes to the signature activity.
                 Intent i = new Intent(context, SignaturePad.class);
-                i.putExtra("receiver_name", receiver_name);
+                i.putExtra("receiver_name", receiverName);
                 i.putExtra("client_id", id);
                 i.putExtra("receiver_id", receiver_id);
-                i.putExtra("phone_number", receiver_phone_number);
-                Log.d("temp", "receiver_id " + receiver_id + " receiver_name " + receiver_name);
-
+                i.putExtra("phone_number", receiverPhoneNumber);
                 context.startActivity(i);
             }
         });
         navigate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {    // On click listener to navigate with google maps to the task location.
                 Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("google.navigation:q=" + address));
+                i.setData(Uri.parse("google.navigation:q=" + receiverAddress));
                 if (ActivityCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
-                        ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) { // check for permission to send SMS.
                     Toast.makeText(context,"Permission for sms not allowed",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                /* Send SMS if permission granted */
                 SmsManager.getDefault().sendTextMessage(receiverPhoneNumber,"","Hello " + receiverName + ",\nIm the delivery boy.\nI'm on my way\nPlease be available on the next 2 hours.\nThank you.",null,null);
                 context.startActivity(i);
             }
@@ -80,24 +78,18 @@ public class TaskCursorAdapter extends CursorAdapter {
         callBtm.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {    // On click listener to call the receiver.
                 Intent intent = new Intent(Intent.ACTION_CALL);
                 intent.setData(Uri.parse("tel:" + receiverPhoneNumber));
-                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) { // if permission not granted.
                     Toast.makeText(context,"Permission for calls not allowed",Toast.LENGTH_SHORT).show();
                     return;
                 }
-                context.startActivity(intent);
+                context.startActivity(intent);  // permission granted and call activity start.
             }
         });
-        time.setText(reciverTime);
-//        date.setText(receiverDate);
-        text.setText(cursor.getString(cursor.getColumnIndex(Constants.TASKS.FULL_NAME)));
-        phone.setText(cursor.getString(cursor.getColumnIndex(Constants.TASKS.PHONE_NUMBER)));
-    }
-
-    @Override
-    public void notifyDataSetChanged() {
-        super.notifyDataSetChanged();
+        time.setText(receiverTime); // Set receiver time to text view.
+        text.setText(receiverName); // Set receiver name to text view.
+        phone.setText(receiverPhoneNumber); // Set receiver phone number to text view.
     }
 }
